f45332e0742712c07f66eb5345d3cc326cf88e17
fawda123 <mbafs2012@gmail.com>
2015-05-04 15:39:28 -0500

    import_local can now read files directly from a zipped folder

--- R/swmpr_retrieval.R
+++ R/swmpr_retrieval.R
@@ -298,7 +298,7 @@ single_param <- function(station_code, param, Max = 100, trace = TRUE){
 #' 
 #' Import local data that were obtained from the CDMO through the zip downloads feature
 #' 
-#' @param  path chr string of full path to .csv files with raw data
+#' @param  path chr string of full path to .csv files with raw data, can be a zipped or unzipped directory
 #' @param  station_code chr string of station to import, typically 7 or 8 characters but may include full name with year, excluding file extension
 #' @param  trace logical indicating if progress is sent to console, default \code{FALSE}
 #' 
@@ -307,7 +307,7 @@ single_param <- function(station_code, param, Max = 100, trace = TRUE){
 #' @return Returns a swmpr object with all parameters and QAQC columns for the station.  The full date range in the raw data are also imported.
 #' 
 #' @details 
-#' The function is designed to import local data that were downloaded from the CDMO outside of R. This approach works best for larger data requests, specifically those from the zip downloads feature in the advanced query section of the CDMO. The function may also work using data from the data export system, but this feature has not been extensively tested (expect bugs). The downloaded data will be in a compressed folder that includes multiple .csv files by year for a given data type (e.g., apacpwq2002.csv, apacpwq2003.csv, apacpnut2002.csv, etc.). The import_local function can be used after the folder is decompressed.  An example dataset is available online to illustrate the format of the data provided through the zip downloads feature.  See the link below to access these data.  All example datasets included with the package were derived from these raw data.
+#' The function is designed to import local data that were downloaded from the CDMO outside of R. This approach works best for larger data requests, specifically those from the zip downloads feature in the advanced query section of the CDMO. The function may also work using data from the data export system, but this feature has not been extensively tested. The downloaded data will be in a compressed folder that includes multiple .csv files by year for a given data type (e.g., apacpwq2002.csv, apacpwq2003.csv, apacpnut2002.csv, etc.). The import_local function can be used to import files directly from the compressed folder or after the folder is decompressed.  In the former case, the requested files are extracted to a temporary directory and then deleted after they are loaded into the current session.  An example dataset is available online to illustrate the format of the data provided through the zip downloads feature.  See the link below to access these data.  All example datasets included with the package were derived from these raw data.
 #' 
 #' Occasionally, duplicate time stamps are present in the raw data.  The function handles duplicate entries differently depending on the data type (water quality,  weather, or nutrients).  For water quality and nutrient data, duplicate time stamps are simply removed.  Note that nutrient data often contain replicate samples with similar but not duplicated time stamps within a few minutes of each other.  Replicates with unique time stamps are not removed but can be further processed using \code{\link{rem_reps}}.  Weather data prior to 2007 may contain duplicate time stamps at frequencies for 60 (hourly) and 144 (daily) averages, in addition to 15 minute frequencies.  Duplicate values that correspond to the smallest value in the frequency column (15 minutes) are retained.  
 #' 
@@ -320,7 +320,7 @@ single_param <- function(station_code, param, Max = 100, trace = TRUE){
 #' @examples
 #' 
 #' \dontrun{
-#' ## this is the path for csv example files
+#' ## this is the path for csv example files, decompressed
 #' path <- 'C:/this/is/my/data/path'
 #'
 #' ## import, do not include file extension
@@ -328,17 +328,39 @@ single_param <- function(station_code, param, Max = 100, trace = TRUE){
 #' }
 import_local <- function(path, station_code, trace = FALSE){
   
-  # sanity check
-  chk_file <- paste0('^', station_code, '.*\\.csv$')
-  if(!any(grepl(chk_file, dir(path)))) 
-    stop('station_code not found in directory')
+  # check if path is zipped
+  zips <- grepl('\\.zip$', path)
   
   ##
   # find station files in path
-  file_nms <- dir(path)
-  expr <- paste0('^', station_code, '.*', '\\.csv$')
-  files_in <- grep(expr, file_nms, value = TRUE)
+  
+  # for zipped
+  if(zips){
+    
+    # get the file names in the zipped folder
+    # check if the requested files exist
+    file_nms <- unzip(path, list = TRUE)$Name
+    expr <- paste0(station_code, '.*', '\\.csv$')
+    files_in <- grep(expr, file_nms, value = TRUE)
+    if(length(files_in) == 0) stop('File(s) not found.')
+    
+    # extract to temporary file
+    tmp_fl <- tempfile()
+    unzip(path, files = files_in, exdir = tmp_fl)
+    files_in <- dir(tmp_fl, recursive = TRUE)
+    
+    # reassign path to temporary file
+    path <- tmp_fl
+    
+  # for unzipped    
+  } else {
 
+    file_nms <- dir(path)
+    expr <- paste0('^', station_code, '.*', '\\.csv$')
+  
+  }
+
+  files_in <- grep(expr, file_nms, value = TRUE)
   if(length(files_in) == 0) stop('File(s) not found.')
 
   station_code <- tolower(station_code)
@@ -353,6 +375,9 @@ import_local <- function(path, station_code, trace = FALSE){
     
     if(trace) cat(file_in, '\t')
     
+    ##
+    # import
+ 
     # import file, try using read.csv, else readlines
     tmp <- try({
       read.csv(file.path(path, file_in), stringsAsFactors = FALSE)
@@ -369,7 +394,7 @@ import_local <- function(path, station_code, trace = FALSE){
         gsub('["\\"]', '', raw[keep_lines[1] - 1]),
         ',')[[1]] 
     }
-      
+    
     names(tmp) <- tolower(names(tmp))
     
     # remove stationcode, isswmp columns
@@ -384,6 +409,9 @@ import_local <- function(path, station_code, trace = FALSE){
     dat[[nm]] <- tmp
     
     }
+  
+  # remove temporary files if zips
+  if(zips) unlink(tmp_fl, recursive = TRUE)
   
   ##
   # column names for each parameter type, used to subset combined data
