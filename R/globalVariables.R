893df640df60c35178e3fbb14b9e531a17dec125
fawda123 <mbafs2012@gmail.com>
2017-05-24 11:27:52 -0500

    fixing build for plot_quants

--- R/globalVariables.R
+++ R/globalVariables.R
@@ -1,1 +1,4 @@
-globalVariables(c('val'))
+globalVariables(c('val', 'X0.', 'X100.', 'X25.', 'X75.', 'monthday', 'quantile'))
+
+#' @importFrom stats quantile
+NULL
--- R/swmpr_analyze.R
+++ R/swmpr_analyze.R
@@ -617,9 +617,9 @@ decomp.default <- function(dat_in, param, date_col, type = 'additive', frequency
 #' @param param chr string of variable to decompose
 #' @param date_col chr string indicating the name of the date column which should be a date or POSIX object.
 #' @param vals_out logical indicating of numeric output is returned, default is \code{FALSE} to return a plot.
-#' @param event logical indicating if an "events" component should be determined
-#' @param type chr string indicating the type of decomposition, either additive (\code{"add"}) or multiplicative (\code{"mult"})
-#' @param center chr string indicating the method of centering, either \code{"mean"} or \code{"median"}
+#' @param event logical indicating if an 'events' component should be determined
+#' @param type chr string indicating the type of decomposition, either additive (\code{'add'}) or multiplicative (\code{'mult'})
+#' @param center chr string indicating the method of centering, either \code{'mean'} or \code{'median'}
 #' @param ... additional arguments passed to or from other methods
 #' 
 #' @concept analyze
@@ -1886,7 +1886,7 @@ map_reserve <- function(nerr_site_id, zoom = 11, text_sz = 6, text_col = 'black'
   
   # plot
   p <- ggmap::ggmap(mapImageData,
-    extent = "panel"
+    extent = 'panel'
       ) + 
     geom_text(data = stats, aes_string(x = 'longitude', y = 'latitude', 
       label= 'station_code'), size = text_sz, colour = text_col
@@ -2002,135 +2002,140 @@ plot_wind.swmpr <- function(swmpr_in, years = NULL, angle = 45, width = 1.5, bre
   
 }
 
-
-######
-
-#' Generate a plot where a single year's data is overlaid on historical data.
+#' Create a plot of data for a single year overlaid on historical data.
 #' 
-#' A line for a single year is plotted over ribbons of historical data quantiles.
+#' A line for a single year is plotted over ribbons ofquantiles for historical data.
 #' 
-#' @param dat_in Input data object.
-#' @param paramtoplot Parameter of interest. Must be in quotation marks and 
-#' must exactly match the column name of the data frame.
-#' @param yr Year to feature as a line on the plot.
-#' @param yrstart Year to begin range of comparison data
-#' @param yrend Year to end range of comparison data
-#' @param yaxislab Label for y-axis. Default is name of \code{paramtoplot}.
-#' @param maintitle Plot title. Default pastes together site name, parameter name, year to feature, and range of years to use for comparison, e.g. "GNDBHWQ 2017 Daily Average Temp overlaid on 2006-2016 daily averages".
-#' @param yrcolor Line color for year of interest. Default is red3.
-#' @param bgcolor1 Color for outer 50\% of data range. Default is lightgray.
-#' @param bgcolor2 Color for middle 50\% of data range. Default is gray65.
-#' @param ... Additional arguments passed to \code{\link[ggplot2]{ggplot}}
+#' @param swmpr_in input swmpr object.
+#' @param paramtoplot chr string of parameter to plot
+#' @param yr numeric of year to feature as a line on the plot
+#' @param yrstart numeric of year to begin range of comparison data
+#' @param yrend numeric of year to end range of comparison data
+#' @param yaxislab chr string for y-axis albel. Default is \code{paramtoplot}.
+#' @param maintitle chr string of plot title. Default pastes together site name, parameter name, year to feature, and range of years to use for comparison, e.g. 'GNDBHWQ 2017 Daily Average Temp overlaid on 2006-2016 daily averages'.
+#' @param yrcolor chr string of line color for year of interest
+#' @param bgcolor1 chr string of color for outer 50\% of data range
+#' @param bgcolor2 chr string of color for middle 50\% of data range.
+#' @param ... additional arguments passed to or from other methods
 #' 
 #' @details 
-#' This function aggregates data into daily average values for the entire time 
-#' series. Then it generates quantiles (min, 25\%, 75\%, max) for each individual 
-#' calendar day (01/01, 01/02, ... 12/31). These quantiles make up a ribbon plot
-#' of historical data, and a line is plotted on top of this to represent data
-#' from a single year in this historical context.
+#' The plot is based on aggregates of daily average values for the entire time series. Quantiles (min, 25\%, 75\%, max) for each individual calendar day (01/01, 01/02, ... 12/31) are used to generate a ribbon plot of historical data and the selected year in \code{yr} is plotted as a line over the ribbon for historical context.
 #' 
 #' required packages: dplyr, lubridate, ggplot2, tibble
-#' assuming data originally imported and processed with SWMPr
 #' 
 #' @author Kim Cressman, Marcus Beck
 #' 
 #' @concept analyze
-
-#' @return An R plot created using ggplot2
-
+#' 
+#' @export
+#'
+#' @return A a \code{\link[ggplot2]{ggplot2}} object.
+#'
+#' @import ggplot2
+#' 
 #' @examples 
-#' # use data from Apalachicola NERR's 
-#' # Cat Point water quality station 
-#' data(apacpwq)
-#' # qaqc the data
-#' apacpwq <- qaqc(apacpwq)
+#' # qaqc
+#' dat <- qaqc(apacpwq)
 #' 
 #' # generate a plot of salinity for 2013 overlaid on 2012-2013 data
-#' quant.plot(apacpwq, 'sal', yr = 2013, yrstart = 2012, yrend = 2013)
+#' plot_quants(dat, 'sal', yr = 2013, yrstart = 2012, yrend = 2013)
 #' 
 #' # change some of the defaults
-#' quant.plot(apacpwq, 'sal', yr = 2013, yrstart = 2012, yrend = 2013, bgcolor1 = "lightsteelblue2", bgcolor2 = "lightsteelblue4", yaxislab = "Salinity (psu)")
-#' 
+#' plot_quants(dat, 'sal', yr = 2013, yrstart = 2012, yrend = 2013, 
+#'  bgcolor1 = 'lightsteelblue2', bgcolor2 = 'lightsteelblue4', 
+#'  yaxislab = 'Salinity (psu)')
+plot_quants <- function(swmpr_in, ...) UseMethod('plot_quants')
+
 #' @rdname plot_quants
 #'
+#' @export
+#' 
 #' @method plot_quants swmpr
-plot_quants <- function(dat_in, paramtoplot, 
-                       yr, yrstart, yrend, 
-                       yaxislab = paramtoplot,
-                       yrcolor = "red3",
-                       bgcolor1 = "lightgray",
-                       bgcolor2 = "gray65",
-                       maintitle = paste0(toupper(attributes(data)$station)," ", yr," Daily Average ",paste0(toupper(substr(paramtoplot, 1, 1)), substr(paramtoplot, 2, nchar(paramtoplot))),"\noverlaid on ", yrstart," - ", yrend, " daily averages"), ...) 
-    
-{
-    
-    # pull out daily averages; name it 'dat2'
-    dat2 <- aggreswmp(dat_in, by='days', FUN='mean')
-    
-    # make a column for just mm-dd, and another column for year
-    # these commands are from the lubridate package
-    dat2$month <- month(dat2$datetimestamp)
-    dat2$day <- day(dat2$datetimestamp)
-    dat2$year <- as.character(year(dat2$datetimestamp))
-    dat2$monthday <- as.character(paste0(dat2$month, "-", dat2$day))
-    
-    # graphing ----
-    
-    # split into feature year (for red) and backdrop years(to all be gray)
-    # this uses the filter() function of dplyr to subset on year
-    dat_feature <- filter(dat2, year == yr)
-    dat_backdrop <- filter(dat2, year >= yrstart & year <= yrend)
-    
-    # work with quantiles ----
-    
-    # do some subsetting and sorting on backdrop data (using dplyr)
-    # need to pull this out so a column can be named 'paramtoplot'
-    # which makes later coding easier
-    dat_quantiles <- select_(dat_backdrop, paramtoplot = paramtoplot, 'monthday', 'year')
-    
-    # by_doy <- group_by(dat_quantiles, monthday)
-    
-    # subset featured data (using dplyr)
-    dat_feature <- select_(dat_feature, paramtoplot = paramtoplot, 'monthday', 'year')
-    
-    # generate a summary table (using dplyr and tibble) ----
-    
-    # first gather quantiles for every monthday
-    # could probably make this quantile(x, n, na.rm=TRUE)
-    # where n is a vector that the user inputs at the beginning of the function to pull out whatever percentiles they want
-    doy_sum2 <- tapply(dat_quantiles$paramtoplot, dat_quantiles$monthday, function(x) quantile(x, c(0, 0.25, 0.75, 1), na.rm=TRUE))
-    # that spits out a list.
-    # pull the list together with do.call and rbind:
-    doy_sum3 <- do.call(rbind, doy_sum2)
-    # but that's a matrix, so make it a data frame:
-    doy_sum3 <- data.frame(doy_sum3)
-    # and turn the row names into the column "monthday", using tibble::rownames_to_column(), and name it doy_sum
-    doy_sum <- rownames_to_column(doy_sum3, var="monthday")
-    
-    # join the two data frames into one (using dplyr)
-    all_doy <- full_join(dat_feature, doy_sum, by = "monthday")
-    
-    # get monthday back into date format by adding a year and then using lubridate's mdy function
-    # using 2008 as the arbitrary year so leap days go where they should
-    all_doy$monthday <- paste0(all_doy$monthday, "-2008")
-    all_doy$monthday <- mdy(all_doy$monthday)
-    
-    # make a year label for the legend
-    yrlabel <- as.character(yr)
-    
-    # make a ribbon plot ----
-    
-    ggplot(all_doy) +
-        geom_ribbon(aes(x=monthday, ymin=X0., ymax=X100., fill ='historical min-max')) +
-        geom_ribbon(aes(x=monthday, ymin=X25., ymax=X75., fill ='historical 25-75 %iles')) +
-        geom_line(aes(x=monthday, y=paramtoplot, color=yrcolor), lwd=1.3) +
-        theme_minimal() +
-        scale_x_date(date_labels = "%m/%d", date_breaks = "1 month", date_minor_breaks="1 month") +
-        theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-        labs(x='Day of Year', 
-             y=yaxislab, 
-             title=maintitle) +
-        scale_color_manual(name='',  values=yrcolor, labels=yrlabel) +
-        scale_fill_manual(name='', values=c('historical min-max' = bgcolor1, 'historical 25-75 %iles' = bgcolor2))
+plot_quants.swmpr <- function(swmpr_in, paramtoplot, yr, yrstart, yrend, yaxislab = NULL, yrcolor = 'red3',   bgcolor1 = 'lightgray', bgcolor2 = 'gray65', maintitle = NULL, ...){
+ 
+  # swmpr attributes
+  station  <- attr(swmpr_in, 'station')
+  timezone <- attr(swmpr_in, 'timezone')
+  
+  # yaxislab is paramtoplot if not provided
+  if(is.null(yaxislab)) yaxislab <- paramtoplot
+  
+  # plot title
+  if(is.null(maintitle))
+    maintitle <- paste0(station, ' ', yr, ' Daily Average ', 
+      paste0(toupper(substr(paramtoplot, 1, 1)), substr(paramtoplot, 2, nchar(paramtoplot))), '\noverlaid on ', 
+      yrstart, ' - ', yrend, ' daily averages')
+  
+  # pull out daily averages; name it 'dat'
+  dat <- aggreswmp(swmpr_in, by = 'days', FUN = 'mean')
+  
+  # make a column for just mm-dd, and another column for year
+  dat$month <- strftime(dat$datetimestamp, '%m')   
+  dat$day <- strftime(dat$datetimestamp, '%d')
+  dat$year <- strftime(dat$datetimestamp, '%Y')
+  dat$monthday <- as.character(paste0(dat$month, '-', dat$day))
+  
+  # graphing ----
+  
+  # split into feature year (for red) and backdrop years(to all be gray)
+  # this uses the filter() function of dplyr to subset on year
+  dat_feature <- dplyr::filter(dat, year == yr)
+  dat_backdrop <- dplyr::filter(dat, year >= yrstart & year <= yrend)
+  
+  # work with quantiles ----
+  
+  # do some subsetting and sorting on backdrop data (using dplyr)
+  # need to pull this out so a column can be named 'paramtoplot'
+  # which makes later coding easier
+  dat_quantiles <- dplyr::select_(dat_backdrop, paramtoplot = paramtoplot, 'monthday', 'year')
+  
+  # by_doy <- group_by(dat_quantiles, monthday)
+  
+  # subset featured data (using dplyr)
+  dat_feature <- dplyr::select_(dat_feature, paramtoplot = paramtoplot, 'monthday', 'year')
+  
+  # generate a summary table (using dplyr and tibble) ----
+  
+  # first gather quantiles for every monthday
+  # could probably make this quantile(x, n, na.rm = TRUE)
+  # where n is a vector that the user inputs at the beginning of the function to pull out whatever percentiles they want
+  doy_sum2 <- tapply(dat_quantiles$paramtoplot, dat_quantiles$monthday, 
+    function(x) quantile(x, c(0, 0.25, 0.75, 1), na.rm = TRUE))
+  # that spits out a list.
+  # pull the list together with do.call and rbind:
+  doy_sum3 <- do.call(rbind, doy_sum2)
+  # but that's a matrix, so make it a data frame:
+  doy_sum3 <- data.frame(doy_sum3)
+  
+  # and turn the row names into the column 'monthday', using tibble::rownames_to_column(), and name it doy_sum
+  doy_sum <- data.frame(monthday = rownames(doy_sum3), doy_sum3, stringsAsFactors = FALSE)
+  
+  # join the two data frames into one (using dplyr)
+  all_doy <- dplyr::full_join(dat_feature, doy_sum, by = 'monthday')
+  
+  # get monthday back into date format by adding a year and then using lubridate's mdy function
+  # using 2008 as the arbitrary year so leap days go where they should
+  all_doy$monthday <- paste0(all_doy$monthday, '-2008')
+  all_doy$monthday <- as.Date(all_doy$monthday, format = '%m-%d-%Y')
+
+  # all_doy$monthday <- 
+  # make a year label for the legend
+  yrlabel <- as.character(yr)
+  
+  # make a ribbon plot ----
+  p <- ggplot(all_doy) +
+    geom_ribbon(aes(x = monthday, ymin = X0., ymax = X100., fill = 'historical min-max')) +
+    geom_ribbon(aes(x = monthday, ymin = X25., ymax = X75., fill = 'historical 25-75 %iles')) +
+    geom_line(aes(x = monthday, y = paramtoplot, color = yrcolor), lwd = 1.3) +
+    theme_minimal() +
+    scale_x_date(date_labels = '%m/%d', date_breaks = '1 month', date_minor_breaks = '1 month') +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    labs(x = 'Day of Year', 
+         y = yaxislab, 
+         title = maintitle) +
+    scale_color_manual(name = '',  values = yrcolor, labels = yrlabel) +
+    scale_fill_manual(name = '', values = c('historical min-max' = bgcolor1, 'historical 25-75 %iles' = bgcolor2))
+  
+  return(p)
     
 }
