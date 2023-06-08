33b8e94cc52e50468d0fd8f4d1884ed32034136e
fawda123 <mbafs2012@gmail.com>
2023-06-08 15:28:46 -0400

    adding some coverage for tests in import_local

--- tests/testthat/test-import_local.R
+++ tests/testthat/test-import_local.R
@@ -10,34 +10,69 @@ sample_data <- data.frame(
 sample_data$datetimestamp <- strftime(sample_data$datetimestamp, format = '%m/%d/%Y %H:%M')
 sample_file <- file.path(test_dir, "apadbwq.csv")
 write.csv(sample_data, file = sample_file, row.names = FALSE)
-
 zip(paste0(test_dir, 'apadwq.zip'), sample_file)
+
+# generate sample met data
+sample_metdata <- data.frame(
+  datetimestamp = seq.POSIXt(as.POSIXct('2022-01-01'), as.POSIXct('2022-01-10'), by = 'hour'),
+  atemp = runif(217, 20, 30),
+  rh = runif(217, 0, 10)
+)
+sample_metdata$datetimestamp <- strftime(sample_metdata$datetimestamp, format = '%m/%d/%Y %H:%M')
+sample_metfile <- file.path(test_dir, "apaebmet.csv")
+write.csv(sample_metdata, file = sample_metfile, row.names = FALSE)
+
+# generate sample nut data
+sample_nutdata <- data.frame(
+  datetimestamp = seq.POSIXt(as.POSIXct('2022-01-01'), as.POSIXct('2023-01-10'), by = 'month'),
+  po4f = runif(13, 20, 30),
+  nh4f = runif(13, 0, 10)
+)
+sample_nutdata <- rbind(sample_nutdata, sample_nutdata[nrow(sample_nutdata), ])
+sample_nutdata$datetimestamp <- strftime(sample_nutdata$datetimestamp, format = '%m/%d/%Y %H:%M')
+sample_nutfile <- file.path(test_dir, "apacpnut.csv")
+write.csv(sample_nutdata, file = sample_nutfile, row.names = FALSE)
 
 # Run the unit tests
 test_that("import_local imports data correctly", {
-  # Test 1: Importing data from a valid path and valid station_code
   result <- import_local(dirname(sample_file), 'apadbwq', trace = T)
   expect_equal(ncol(result), 3)
   expect_equal(nrow(result), 217)
   expect_s3_class(result, 'swmpr')
 })
 
+test_that("import_local imports met data correctly", {
+  result <- import_local(dirname(sample_metfile), 'apaebmet', trace = T)
+  expect_equal(ncol(result), 3)
+  expect_equal(nrow(result), 217)
+  expect_s3_class(result, 'swmpr')
+})
+
+test_that("import_local imports nut data correctly", {
+  result <- import_local(dirname(sample_nutfile), 'apacpnut', trace = T)
+  expect_equal(ncol(result), 3)
+  expect_equal(nrow(result), 12)
+  expect_s3_class(result, 'swmpr')
+})
+
 test_that("import_local raises an error for invalid path", {
-  # Test 2: Importing data from an invalid path
   expect_error(import_local("invalid_path.csv", "apadbwq"), "Path does not exist")
 })
 
 test_that("import_local raises an error for invalid station_code", {
-  # Test 3: Importing data with an invalid station_code
   expect_error(import_local(dirname(sample_file), "invalid_station"), "station_code must include wq, met, or nut")
 })
 
 test_that("import_local imports zip data correctly", {
-  # Test 1: Importing data from a valid path and valid station_code
-  result <- import_local(paste0(test_dir, 'apadwq.zip'), 'apadbwq', trace = T)
+  result <- import_local(paste0(test_dir, 'apadwq'), 'apadbwq', trace = T)
   expect_equal(ncol(result), 3)
   expect_equal(nrow(result), 217)
   expect_s3_class(result, 'swmpr')
+})
+
+test_that("import_local returns error if files not found", {
+  expect_error(import_local(paste0(test_dir, 'apadwq'), 'asdfwq', trace = T), "File(s) not found.")
+  expect_error(import_local(dirname(sample_file), 'asdfwq', trace = T), "File(s) not found.")
 })
 
 # Cleanup: Remove the temporary directory
